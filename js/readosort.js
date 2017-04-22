0d77d6331cc7279d742edcf78183ca5722b9160c
Rishabh Verma <me@rishabhverma.me>
2017-04-22 18:08:06 +0530

    xhr request code

--- js/readosort.js
+++ js/readosort.js
@@ -4,3 +4,13 @@ urlEls.forEach(function(el, index){
   urls.push(el.getAttribute("href"));
 })
 console.log(urls);
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       console.log(xhttp.responseText);
+    }
+};
+xhttp.open("GET", "https://facebook.com", true);
+xhttp.send();
